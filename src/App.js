1d6fa85399be7dd37c57ab9e7c101af76fcec7c8
samnewga <newgate90s@gmail.com>
2020-03-02 21:58:53 -0700

    currency test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RegistrationForm from './registrationForm';
+import Currency from './Currency';
+import CurrencyError from './CurrencyError';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <RegistrationForm />
+
+        <CurrencyError>
+          Germany: <Currency value={21} locale="de-DE" currency="USD" />
+          USA: <Currency value={21} locale="en-US" currency="USD" />
+        </CurrencyError>
+      </div>
+    );
+  }
 }
 
 export default App;
